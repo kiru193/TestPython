66a5440d94fd7276fa963ab707f641ca9a225fa9
DESKTOP-FUK7EI4\zi-ku <zi-kuvurumu10922@hotmail.co.jp>
2023-05-07 14:00:19 +0900

    コメントアウトの仕方 #で単一行のコメントアウト """で囲った部分はすべてコメントアウトされる

--- Learning_python/Learning_python/Learning_python.py
+++ Learning_python/Learning_python/Learning_python.py
@@ -1,8 +1,6 @@
 
+print('Hello World!')#単一行コメントアウト
+"""
+複数行のコメントアウト
 print('Hello World!')
-print(2*5)
-name = 'SENA'
-print('Hello', name, 'さん!')
-print('Hello', name, 'さん!',sep = '/')
-print('Hello', end = '/')
-print(name)
+"""
