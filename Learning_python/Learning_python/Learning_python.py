57ee794021f77e7114c8534ca1479e029e45579c
DESKTOP-FUK7EI4\zi-ku <zi-kuvurumu10922@hotmail.co.jp>
2023-05-11 00:30:52 +0900

    アンバック代入を利用した変数のスワッピングをすることができる C言語のようにもう一つ変数を用意してーーというのをしなくてよい

--- Learning_python/Learning_python/Learning_python.py
+++ Learning_python/Learning_python/Learning_python.py
@@ -1,38 +1,7 @@
-#アンバック代入
+#アンバック代入を利用した変数のスワッピング
+x = 10
+y = 20
 
-data = [1,2,3,4,5]
-a,b,c,d,e = data
-print(a,b,c,d,e)
-print()
-
-#アンバック代入の応用、特定の要素だけを取り出してまとめて代入する
-m,n,*o = data
-print(m,n,o)
-print()
-
-m,*n,o = data
-print(m,n,o)
-print()
-
-*m,n,o = data
-print(m,n,o)
-print()
-
-#アンバック代入の応用、一部の要素を切り捨てる
-a,_,b,_,c = data
-print(a,b,c)
-print(_)
-print()
-
-a,*_,b = data
-print(a,b)
-print()
-
-#入れ子リストのアンバック
-data = [1,2,[31,32,33]]
-a,b,c = data
-print(a,b,c)
-print()
-
-x,y,(z1,z2,z3) = data
-print(x,y,z1,z2,z3)
+print(x,y)
+x,y = y,x
+print(x,y)
