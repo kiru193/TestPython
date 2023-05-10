09857a1f350707dea365b3c238020a0859fbcfd7
DESKTOP-FUK7EI4\zi-ku <zi-kuvurumu10922@hotmail.co.jp>
2023-05-11 00:28:34 +0900

    アンバックと呼ばれる手法 リストの要素を各変数に代入する方法 アンダーバーは一部の要素を切り捨てるではなく、変数＿に代入しているだけであるので注意 当然リストの中央の一部を切り取ってくるのは無理よ

--- Learning_python/Learning_python/Learning_python.py
+++ Learning_python/Learning_python/Learning_python.py
@@ -1,12 +1,38 @@
-data1 =[1,2,3]
-data2 = data1
-data1[0] = 100
-print(data1)
-print(data2)
+#アンバック代入
 
-x=1
-y=x
+data = [1,2,3,4,5]
+a,b,c,d,e = data
+print(a,b,c,d,e)
+print()
 
-x +=10
-print(x)
-print(y)
+#アンバック代入の応用、特定の要素だけを取り出してまとめて代入する
+m,n,*o = data
+print(m,n,o)
+print()
+
+m,*n,o = data
+print(m,n,o)
+print()
+
+*m,n,o = data
+print(m,n,o)
+print()
+
+#アンバック代入の応用、一部の要素を切り捨てる
+a,_,b,_,c = data
+print(a,b,c)
+print(_)
+print()
+
+a,*_,b = data
+print(a,b)
+print()
+
+#入れ子リストのアンバック
+data = [1,2,[31,32,33]]
+a,b,c = data
+print(a,b,c)
+print()
+
+x,y,(z1,z2,z3) = data
+print(x,y,z1,z2,z3)
